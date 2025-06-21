67c600d9b5986530182914567495ab1022426ce0
Pablojonh6550 <pablojonh6550@gmail.com>
2025-06-21 17:01:38 -0300

    refactor: add go back button

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -13,20 +13,18 @@
                         <div class="mb-3">
                             <label for="name" class="form-label">Nome</label>
                             <input type="text" class="form-control" id="name" name="name">
-                            {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> --}}
                         </div>
                         <div class="mb-3">
                             <label for="contact" class="form-label">Contato</label>
                             <input type="number" min="0" class="form-control" id="contact" name="contact_number">
-                            {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> --}}
                         </div>
                         <div class="mb-3">
                             <label for="email" class="form-label">Email</label>
                             <input type="text" class="form-control" id="email" name="email">
-                            {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> --}}
                         </div>
                         <button type="submit" class="btn btn-primary w-100">Salvar</button>
                     </form>
+                    <a href="{{ route('index') }}" class="btn btn-secondary mt-2 w-100">Voltar</a>
                 </div>
             </div>
         </div>
