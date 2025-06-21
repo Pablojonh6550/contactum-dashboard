82a19bacd19a0e94738a8bf5ee4319eed2ea026e
Pablojonh6550 <pablojonh6550@gmail.com>
2025-06-21 15:42:46 -0300

    refactor: register contact repository and interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 use App\Repositories\BaseRepository;
 use App\Interfaces\BaseInterface;
+use App\Interfaces\Contact\ContactInterface;
+use App\Repositories\Contact\ContactRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(BaseInterface::class, BaseRepository::class);
+        $this->app->bind(ContactInterface::class, ContactRepository::class);
     }
 
     /**
